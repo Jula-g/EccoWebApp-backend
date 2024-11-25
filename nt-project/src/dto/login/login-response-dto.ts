f3f8a4ef3cc0d7c6c2b354476c9d2b23488f3bd0
Julia Galant <276068@student.pwr.edu.pl>
2024-11-25 23:17:57 +0100

    Login and Register dtos added.

---
+++ nt-project/src/dto/login/login-response-dto.ts
@@ -0,0 +1,10 @@
+export class LoginResponseDto {
+    token : string;
+    userId : string;
+
+    constructor(token : string, userId : string) {
+        this.token = token;
+        this.userId = userId;
+    }
+
+}
